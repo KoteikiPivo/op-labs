b1d6f4c1dd93db04b8db5552706e14fd091c81cd
sqbe <bobkov005@gmail.com>
2024-11-22 01:01:33 +0300

    Added 'to_json' function for all classes

--- classes.py
+++ classes.py
@@ -1,10 +1,8 @@
-
 class Person:
     """Базовый класс для людей"""
     def __init__(self, name: str, phone: int):
         self.name = name
         self.phone = phone
-
 
 class Employee(Person):
     """Базовый класс для рабочих"""
@@ -19,6 +17,15 @@ class Librarian(Employee):
     def __init__(self, name: str, phone: int, work_id: int, work_days: str):
         super().__init__(name, phone, work_id)
         self.work_days = work_days
+    
+    def to_json(self):
+        return {
+            'name': self.name,
+            'phone': self.phone,
+            'work_id': self.work_id,
+            'work_days': self.work_days
+        }
+
 
 
 class Member(Person):
@@ -26,6 +33,14 @@ class Member(Person):
     def __init__(self, name: str, phone: int, member_id: int):
         super().__init__(name, phone)
         self.member_id = member_id
+        
+    def to_json(self):
+        return {
+            'name': self.name,
+            'phone': self.phone,
+            'member_id': self.member_id
+        }
+
 
 class Account(Member):
     """Классы аккаунтов электронной библиотеки"""
@@ -35,12 +50,27 @@ class Account(Member):
         self.login = login
         self.passwd = passwd
 
+    def to_json(self):
+        return {
+            'name': self.name,
+            'phone': self.phone,
+            'member_id': self.member_id,
+            'login': self.login,
+            'passwd': self.passwd
+        }
 
 class Customer(Person):
     """Класс клиента доставки"""
     def __init__(self, name: str, phone: int, address: str):
         super().__init__(name, phone)
         self.address = address
+    
+    def to_json(self):
+        return {
+            'name': self.name,
+            'phone': self.phone,
+            'address': self.address
+        }
 
 
 
@@ -51,11 +81,26 @@ class Book:
         self.author = author
         self.year = year
 
+    def to_json(self):
+        return {
+            'title': self.title,
+            'author': self.author,
+            'year': self.year
+        }
+
 class EBook(Book):
     """Классы электронных книг библиотеки"""
     def __init__(self, title: str, author: str, year: int, cost: int):
         super().__init__(title, author, year)
         self.cost = cost
+    
+    def to_json(self):
+        return {
+            'title': self.title,
+            'author': self.author,
+            'year': self.year,
+            'cost': self.cost
+        }
 
 
 
@@ -66,9 +111,24 @@ class Order:
         self.customer = customer
         self.ord_id = ord_id
 
+    def to_json(self):
+        return {
+            'book': self.book.to_json,
+            'customer': self.customer.to_json,
+            'ord_id': self.ord_id
+        }
+
 class DeliveryDriver(Employee):
     """Класс доставщиков"""
     def __init__(self, name: str, phone: int, work_id: int, order: Order):
         super().__init__(name, phone, work_id)
         self.work_id = work_id
         self.order = order
+
+    def to_json(self):
+        return {
+            'name': self.name,
+            'phone': self.phone,
+            'work_id': self.work_id,
+            'order': self.order.to_json
+        }
