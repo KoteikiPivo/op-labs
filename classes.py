c1b2e0a39c6fb2d30ae20f5bc7ef213c2d363347
sqbe <bobkov005@gmail.com>
2024-11-21 21:05:26 +0300

    Finished adding 10 classes to classes.py

--- classes.py
+++ classes.py
@@ -1,28 +1,67 @@
 # Базовый класс для людей
 class Person:
-    def __init__(self, name, phone):
+    def __init__(self, name: str, phone: int):
         self.name = name
         self.phone = phone
 
 # Базовый класс для рабочих
 class Employee(Person):
-    def __init__(self, name, phone, work_id):
-        super.__init__(name, phone)
+    def __init__(self, name: str, phone: int, work_id: int):
+        super().__init__(name, phone)
         self.work_id = work_id
 
+
+# Классы рабочих
 class Librarian(Employee):
-    def __init__(self, name, phone, work_id, work_days):
-        super.__init__(name, phone, work_id)
+    def __init__(self, name: str, phone: int, work_id: int, work_days: str):
+        super(Employee, self).__init__(name, phone, work_id)
         self.work_days = work_days
 
 class DeliveryDriver(Employee):
-    def __init__(self, name, phone, work_id, order):
-        super.__init__(name, phone, work_id)
+    def __init__(self, name: str, phone: int, work_id: int, order: Order):
+        super(Employee, self).__init__(name, phone, work_id)
         self.work_id = work_id
         self.order = None
 
-    def give_order(self, new_order):
+    def give_order(self, new_order: Order):
         self.order = new_order
 
-#class Member(Person):
-    #def __init__(self, name, phone, )
+
+# Классы членов библиотеки
+class Member(Person):
+    def __init__(self, name: str, phone: int, member_id: int):
+        super().__init__(name, phone)
+        self.member_id = member_id
+
+class Account(Member):
+    def __init__(self, name: str, phone: int, member_id: int, login: str, passwd: str):
+        super(Member, self).__init__(name, phone, member_id)
+        self.login = login
+        self.passwd = passwd
+
+# Класс клиента доставки
+class Customer(Person):
+    def __init__(self, name: str, phone: int, address: str):
+        super().__init__(name, phone)
+        self.address = address
+
+
+# Классы книг библиотеки
+class Book:
+    def __init__(title: str, author: str, year: int):
+        self.title = title
+        self.author = author
+        self.year = year
+
+class EBook(Book):
+    def __init__(self, title: str, author: str, year: int, cost: int):
+        super().__init__(title, author, year)
+        self.cost = cost
+
+
+# Класс заказов
+class Order:
+    def __init__(book: Book, member: Member):
+        self.book = book
+        self.member = member
+
