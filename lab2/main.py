96b1cf570afdf167dd74fcd8f1c0defecb00de3b
sqbe <bobkov005@gmail.com>
2024-11-30 00:32:07 +0300

    Added unit testing, changed regex to prevent mixing '-' and ':' in addresses

--- lab2/main.py
+++ lab2/main.py
@@ -1,19 +1,25 @@
 import re
 import requests
 
-PATTERN = r'\b(?:[0-9A-F]{2}[:-]){5}(?:[0-9A-F]){2}\b'
+# PATTERN = r'\b(?:[0-9A-F]{2}[:-]){5}(?:[0-9A-F]){2}\b'
+PATTERN = (r'\b(?:(?:[0-9A-F]{2}[:]){5}(?:[0-9A-F]){2})\b|'
+           r'\b(?:(?:[0-9A-F]{2}[-]){5}(?:[0-9A-F]){2})\b')
 LINK = 'https://en.wikipedia.org/wiki/MAC_address'
+
+
+def mac_return_valid(mac: str):
+    return re.findall(PATTERN, mac, flags=re.I)
 
 
 def mac_from_file(file_name: str):
     with open(file_name, 'r', encoding='utf-8') as file:
-        return re.findall(PATTERN, file.read(), flags=re.I)
+        return mac_return_valid(file.read())
     return None
 
 
 def mac_from_link(link: str):
     link_text = requests.get(link).text
-    return re.findall(PATTERN, link_text, flags=re.I)
+    return mac_return_valid(link_text)
 
 
 def main():
@@ -21,4 +27,5 @@ def main():
     print(mac_from_link(LINK))
 
 
-main()
+if __name__ == "__main__":
+    main()
