61d01c4e454c7d81db4cf00d2d86a57558af9254
sqbe <bobkov005@gmail.com>
2024-11-29 20:44:16 +0300

    Added a function to get all MAC addresses found on a web page, cleaned up some code

--- lab2/main.py
+++ lab2/main.py
@@ -1,25 +1,24 @@
 import re
 import requests
 
-
-def check_mac(mac):
-    return re.match(r'([0-9A-F]{2}[:]){5}[0-9A-F]{2}|'
-                    r'([0-9A-F]{2}[-]){5}[0-9A-F]{2}',
-                    mac, flags=re.IGNORECASE) is not None
+PATTERN = r'\b(?:[0-9A-F]{2}[:-]){5}(?:[0-9A-F]){2}\b'
+LINK = 'https://en.wikipedia.org/wiki/MAC_address'
 
 
 def mac_from_file(file_name: str):
-    mac_list = []
     with open(file_name, 'r', encoding='utf-8') as file:
-        for i in file:
-            i = i.strip("\n")
-            if check_mac(i):
-                mac_list.append(i)
-    return mac_list
+        return re.findall(PATTERN, file.read(), flags=re.I)
+    return None
+
+
+def mac_from_link(link: str):
+    link_text = requests.get(link).text
+    return re.findall(PATTERN, link_text, flags=re.I)
 
 
 def main():
     print(mac_from_file("lab2/test_macs.txt"))
+    print(mac_from_link(LINK))
 
 
 main()
